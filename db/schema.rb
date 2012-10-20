00fdab8a1ea52d81e9e0878d590486e416eff025
Hajime Sato <von_mises@cosmo.plala.or.jp>
2012-10-20 23:14:09 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111227115841) do
+ActiveRecord::Schema.define(:version => 20121020140715) do
 
   create_table "line_items", :force => true do |t|
     t.integer  "order_id"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(:version => 20111227115841) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "ordered_at"
+  end
+
+  create_table "names", :force => true do |t|
+    t.string   "sei",        :default => ""
+    t.string   "mei",        :default => ""
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "orders", :force => true do |t|
