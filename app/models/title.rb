05da807a8e40b0469483b2d00b63083685a2a1d6
Hajime Sato <von_mises@cosmo.plala.or.jp>
2012-11-02 06:56:31 +0900

    titles に追加した priority への対応．

--- app/models/title.rb
+++ app/models/title.rb
@@ -2,6 +2,6 @@ class Title < ActiveRecord::Base
 	has_many :products
 
 	def self.find_titles_for_sale
-		where(["on_sale = ?", true])
+		where(["on_sale = ?", true]).order("priority, id")
 	end
 end
